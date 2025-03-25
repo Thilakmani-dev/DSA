42e3a32e0d703ea2283dd48d412d22b9ad7521fc
Thilakmani <71221182+Thilakmani-dev@users.noreply.github.com>
2025-03-25 10:13:18 +0530

    using single for loop and map

--- NewSum.js
+++ NewSum.js
@@ -17,3 +17,25 @@ function twoSum(input, target){
 
 const result = twoSum(numbers, 7);
 console.log(result);
+
+
+
+
+
+//using single for loop and map
+const numbers1 = [7, 2, 4, 10, 3];
+
+function getTwoSum(nums, target) {
+  let map = new Map();
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      result[0] = i;
+      result[1] = map.get(target - nums[i]);
+    }
+    map.set(nums[i], i);
+  }
+  return result;
+}
+
+console.log(getTwoSum(numbers1, 9)); // [3,1]
